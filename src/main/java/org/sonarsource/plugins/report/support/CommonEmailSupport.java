50932704f8befb6f1f0f47936e76ca08331abe7b
YangCG <yangchengang2@csvw.com>
2018-09-06 15:20:06 +0800

    remove sonar-ws from pom.xml

--- src/main/java/org/sonarsource/plugins/report/support/CommonEmailSupport.java
+++ src/main/java/org/sonarsource/plugins/report/support/CommonEmailSupport.java
@@ -1,20 +1,17 @@
 package org.sonarsource.plugins.report.support;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.mail.EmailAttachment;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.MultiPartEmail;
+import org.apache.commons.mail.SimpleEmail;
 
+@Slf4j
 public class CommonEmailSupport {
 
     public static void sendEmailWithAttachments(String host, String username, String password, String name,
                                                 String fromAddress, String[] toAddress, String subject, String message, String attachFile) throws EmailException {
-        // Create the attachment
-        EmailAttachment attachment = new EmailAttachment();
-        attachment.setPath(attachFile);
-        attachment.setDisposition(EmailAttachment.ATTACHMENT);
-        attachment.setDescription("SonarQube analysis result");
-
         // Create the email message
         MultiPartEmail email = new MultiPartEmail();
         email.setHostName(host);
@@ -26,19 +23,27 @@ public class CommonEmailSupport {
                 .addTo(toAddress)
                 .setSubject(subject)
                 .setMsg(message);
+        email.setSocketConnectionTimeout(30000);
+        email.setSocketTimeout(30000);
+        email.setCharset("utf-8");
 
         // add the attachment
-        email.attach(attachment);
+        if (StringUtils.isNotEmpty(attachFile)) {
+            EmailAttachment attachment = new EmailAttachment();
+            attachment.setPath(attachFile);
+            attachment.setDisposition(EmailAttachment.ATTACHMENT);
+            attachment.setDescription("SonarQube analysis result");
+            email.attach(attachment);
+        }
         // send the email
         email.send();
-
     }
 
 
-    public static void sendEmail(String host, String username, String password, String name,
-                                 String fromAddress, String[] toAddress, String subject, String message, String attachFile) throws EmailException {
+    public static void sendSimpleEmail(String host, String username, String password, String name,
+                                       String fromAddress, String[] toAddress, String subject, String message) throws EmailException {
         // Create the email message
-        MultiPartEmail email = new MultiPartEmail();
+        SimpleEmail email = new SimpleEmail();
         email.setHostName(host);
         if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
             email.setAuthentication(username, password);
@@ -47,11 +52,13 @@ public class CommonEmailSupport {
         email.setFrom(fromAddress, name)
                 .addTo(toAddress)
                 .setSubject(subject)
-                .setMsg(message + "\n No attachments");
-
+                .setMsg(message);
+        email.setSocketConnectionTimeout(30000);
+        email.setSocketTimeout(30000);
+        email.setCharset("utf-8");
         // send the email
         email.send();
-
     }
+
 
 }
